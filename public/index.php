e5f4f3a906d03967154ba5aa38bf171eb0cc084d
Dopethroned <a_anund@hotmail.com>
2016-05-11 17:26:54 +0200

    start work on landing page

---
+++ public/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Case</title>
+	<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<!-- Normalize non-sass -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.1.1/normalize.min.css" type="text/css">
+<!-- Latest compiled and minified Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+<!-- Custom CSS -->
+	<link rel="stylesheet" href="css/style.css"type="text/css">
+</head>
+<body>
+
+<div class="container-fluid">
+
+	<div class="row">
+
+		<div class="col-xs-12 page-cnt">
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2">
+					<div class="row">
+						<div class="col-xs-12  col-sm-6 col-sm-offset-3 logo-cnt">
+							<img class="img-responsive center-block"src="img/logo.png" alt="Retail United logo">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12 slogan">
+							<p>Fullservicebyrån inom retail som hjälper varumärken att bli valda.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-sm-offset-3 clips-cnt">
+					<div class="row">
+						<div class="col-xs-12 col-sm-3 col-sm-offset-1 clip-cnt">
+							<div class="row">
+								<a href="#">
+									<div class="col-xs-4 col-sm-12">
+										<img class="img-responsive center-block clip" src="img/red-clip.png" alt="Red paper clip">
+									</div>
+									<div class="col-xs-8 col-sm-12">
+										<p>Om oss</p>
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-3 col-sm-offset-1 clip-cnt">
+							<div class="row">
+								<a href="#">
+									<div class="col-xs-4 col-sm-12">
+										<img class="img-responsive center-block clip" src="img/blue-clip.png" alt="Blue paper clip">
+									</div>
+									<div class="col-xs-8 col-sm-12">
+										<p>Kundcase</p>
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-3 col-sm-offset-1 clip-cnt">
+							<div class="row">
+								<a href="#">
+									<div class="col-xs-4 col-sm-12">
+										<img class="img-responsive center-block clip" src="img/green-clip.png" alt="Green paper clip">
+									</div>
+									<div class="col-xs-8 col-sm-12">
+										<p>Kontakt</p>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> <!-- /main col -->
+	</div> <!-- / main row -->
+</div>
+
+<!-- Latest compiled and minified Bootstrap JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!-- jQuery -->
+	<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<!-- Green Sock -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.3/TweenMax.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.3/plugins/ScrollToPlugin.min.js"></script>
+<!-- Custom Script -->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
